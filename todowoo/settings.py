6d3ae38f31b136206597b6f2aca142aa54c1ab03
kumalishwor <ishworkumal58530@gmail.com>
2023-07-02 22:58:39 +0545

    added media root and media url

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -129,6 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 LOGIN_URL = '/login'
