2b83b253093f205c30355dfdf61dd24733e3f170
kumalishwor <ishworkumal58530@gmail.com>
2023-07-03 01:44:52 +0545

    remove whitenoise

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -53,7 +53,6 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
