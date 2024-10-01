96143c0f8d5ad80962f612675b32b445dfcfe724
Ishwor Kumal <ishworkumal@Ishwors-MacBook-Air.local>
2024-10-01 23:01:09 +1300

    giving permission chmod to sh file

---
+++ set_permission.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Giving executable permission to entrypoint.sh
+chmod +x entrypoint.sh
+
+# Giving executable permission to collectstatic.sh
+chmod +x scripts/collectstatic.sh
+
+# Giving executable permission to migrate.sh
+chmod +x scripts/migrate.sh
+
+# You can add more files here if needed
+echo "Permissions have been updated successfully!"
